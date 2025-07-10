4987c12ef57ef673311c02e808e94722f349c777
himanshudube97 <himanshu.dube13@gmail.com>
2025-07-10 23:55:27 +0530

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,10 +15,9 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-explicit-any": "warn",
-      "typescript-eslint/explicit-function-return-type": "warn",
+      "@typescript-eslint/no-empty-function": "warn",
       "react/prop-types": "warn",
       "no-unused-vars": "warn",
-      "no-console": "warn"
     }
   }
 ];
