fff8271bf7b12819eaa0bb16bb018f64853573d3
pradnk <pradnk@gmail.com>
2025-10-17 13:31:09 +0530

    Retaining public embed and removing test code for private embed

--- app/embed/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function EmbedLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className="min-h-screen">{children}</body>
-    </html>
-  );
-}
