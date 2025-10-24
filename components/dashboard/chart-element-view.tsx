fc7666dea01668fdc8e724aabc6adeb169d90c5b
pradnk <pradnk@gmail.com>
2025-10-24 13:57:35 +0530

    Resolved height difference in edit vs preview mode for table charts

--- components/dashboard/chart-element-view.tsx
+++ components/dashboard/chart-element-view.tsx
@@ -1166,7 +1166,7 @@ export function ChartElementView({
         <div
           ref={tableRef}
           className={cn(
-            'w-full flex-1 min-h-[500px] p-4 flex flex-col',
+            'w-full p-4 overflow-auto',
             isFullscreen && '!h-full !min-h-[90vh] !bg-white p-4'
           )}
           style={{
